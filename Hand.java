e227fc93334c2505a396f840d7765eb7a88053c7
kznk7 <56796881+kznk7@users.noreply.github.com>
2022-01-20 14:17:28 +0900

    Hand.java

--- Hand.java
+++ Hand.java
@@ -2,13 +2,15 @@ public class Hand {
     public static final int GU = 1;
     public static final int CHOKI = 2;
     public static final int PA = 3;
+
     public static String getHandName(int hand) {
         String handName = "";
 
-        //課題2 Playerの手の決定とその表示
+        //課題2(bot) Playerの手の決定とその表示
         //1ならば"グー",2ならば"チョキ",3ならば"パー"を返す
-
-
+        if (hand == GU) handName = "グー";
+        else if (hand == CHOKI) handName = "チョキ";
+        else if (hand == PA) handName = "パー";
         return handName;
     }
 }
