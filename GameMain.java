c334b970322dd6308183696c74326a4cf744dea7
kznk7 <56796881+kznk7@users.noreply.github.com>
2022-01-20 14:17:26 +0900

    GameMain.java

--- GameMain.java
+++ GameMain.java
@@ -4,18 +4,16 @@ public class GameMain {
         System.out.println("1:グー 2:チョキ 3:パー");
         System.out.println("じゃんけん...");
 
-        //課題2 Playerの手の決定とその表示
+        //課題2(Bot) Playerの手の決定とその表示
         //PlayerクラスのdecidesPlayerHandメソッドとHandクラスのgetHandNameメソッドを使ってPlayerの手を表示する
-
-        //int playerHand =
+        int playerHand = Player.decidesPlayerHand();
+        System.out.println("プレイヤー : " + Hand.getHandName(playerHand));
 
         //課題3 Computerの手の決定とその表示
         //ComputerクラスのdecidesComputerHandメソッドとHandクラスのgetHandNameメソッドを使ってPlayerの手を表示する
-
         //int computerHand =
 
-        //課題4 勝敗判定とその表示
+        //課題4(Bot) 勝敗判定とその表示
         //課題2,3において実装したPlayerおよびComputerの手の表示はVictoryOrDefeatクラスに移動する
-
     }
 }
